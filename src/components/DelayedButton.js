7c820d5212b24f68f5864cca461221ecc8d8c676
Madeline-Stark <madeline.stark1@gmail.com>
2019-10-01 15:24:05 -0700

    Update DelayedButton.js

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -5,7 +5,7 @@ export default class DelayedButton extends Component {
 handleClick = (event) => {
   //function passed in as prop
   //need arrow function below to preserve this to access this.props.onDelayedClick
-  event.persist() //need to persist b/c of pooling
+  event.persist() //need to persist b/c of pooling/because passing it to an anonymous arrow function below
   setTimeout(() => this.props.onDelayedClick(event), this.props.delay)
 }
 
